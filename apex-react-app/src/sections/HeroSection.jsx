3431bd7b65841c194e547caf7495bd1a9460d395
Wearsolo <chinnaphat.sri@kkumail.com>
2025-06-12 18:11:08 +0700

    ปีศาจมาร vs พยานาก

--- apex-react-app/src/sections/HeroSection.jsx
+++ apex-react-app/src/sections/HeroSection.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react';
 import banner1 from '../assets/img/banner1.png';
 import banner2 from '../assets/img/banner2.png';
 import banner3 from '../assets/img/banner3.png';
+import bg1 from '../assets/img/พื้นหลังพญานาค.png';
+import apex from '../assets/img/ปีศาจมาร.png';
 
 const banners = [banner1, banner2, banner3];
 
@@ -15,9 +17,8 @@ const HeroSection = () => {
 
     return () => clearInterval(timer);
   }, []);
-  return (
-    <div className="relative w-full overflow-hidden bg-[#1E1B3C]">
-      <div className="container mx-auto">
+  return (    <div className="relative w-full overflow-hidden bg-[#1E1B3C]">
+      <div className="container mx-auto flex flex-col">
         <div className="relative w-full h-[calc(100vh-48px)]">
           {banners.map((banner, index) => (
             <div
@@ -32,8 +33,7 @@ const HeroSection = () => {
                   alt={`Banner ${index + 1}`}
                   className="max-w-full max-h-full object-contain"
                 />
-                {/* Navigation dots - moved inside the banner container */}
-                <div className="absolute bottom-40 left-1/2 transform -translate-x-1/2 flex space-x-4 z-20 px-4 py-2 rounded-full">
+                {/* Navigation dots - moved inside the banner container */}                <div className="absolute bottom-[200px] left-1/2 transform -translate-x-1/2 flex space-x-4 z-20 px-4 py-2 rounded-full">
                   {banners.map((_, dotIndex) => (
                     <button
                       key={dotIndex}
@@ -42,11 +42,20 @@ const HeroSection = () => {
                       } hover:bg-[#B05AE7]`}
                       onClick={() => setCurrentBanner(dotIndex)}
                     />
-                  ))}
-                </div>
-              </div>
-            </div>
-          ))}
+                  ))}                </div>              </div>
+            </div>          ))}
+        </div>        <div className="w-full -mt-48 relative">
+          <img
+            src={bg1}
+            alt="พญานาค"
+            className="w-full h-auto object-contain"
+          />
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">            <img
+              src={apex}
+              alt="Apex"
+              className="w-[800px] h-auto"
+            />
+          </div>
         </div>
       </div>
     </div>
