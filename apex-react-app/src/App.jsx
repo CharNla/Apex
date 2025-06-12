4a5995b24e4b98c504209b5021749d6a685654b3
Wearsolo <chinnaphat.sri@kkumail.com>
2025-06-12 12:01:29 +0700

    Initial commit

---
+++ apex-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="text-center">
+      <h1 className="text-3xl font-bold text-blue-500">
+        Hello, Tailwind CSS!
+      </h1>
+      <p className="mt-4">
+        This is a React app with Tailwind CSS.
+      </p>
+    </div>
+  );
+}
+
+export default App;
