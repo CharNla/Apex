4a4954c1656d98a57cedf8749f7550411f584545
teeraphat-k <nuania715120@gmail.com>
2025-06-13 16:28:11 +0700

    add font

--- apex-react-app/tailwind.config.js
+++ apex-react-app/tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Poppins'],
+        sans: ['Prompt', ...defaultTheme.fontFamily.sans],
       },
       colors: {
         'light-lavender': '#F2EEF8',
