c51e8737b79d4c41d2111817f84e0d441a5c6922
Wearsolo <chinnaphat.sri@kkumail.com>
2025-06-17 10:51:06 +0700

    fix

--- apex-react-app/vite.config.js
+++ apex-react-app/vite.config.js
@@ -4,6 +4,7 @@ import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Apex/',
   plugins: [
     react(),
     ViteImageOptimizer({
